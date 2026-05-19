626a1bdb4d27faa26828793388eedf50669a868e
Cristian Torres <torrescc@globalhitss.com>
2026-05-19 18:27:13 -0500

    Se quita la dependencia de ssl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -14,7 +13,7 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), tailwindcss(), basicSsl(), VitePWA({
+  plugins: [react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
     manifest: {
